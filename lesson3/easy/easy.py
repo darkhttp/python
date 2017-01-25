834e3417b895b182033028fedcd2bc4be10e07dc
darkhttp <dimaanisov24@gmail.com>
2017-01-25 17:41:02 +0300

    Рабочая версия программы

--- lesson3/easy/easy.py
+++ lesson3/easy/easy.py
@@ -3,25 +3,37 @@
 # до кол-ва знаков (кол-во знаков передается вторым аргументом)
 # Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
 # Для решения задачи не используйте встроенные и функции и функции из модуля math
+# Задание-1:
+# Напишите функцию округлящую полученное произвольное десятичное число,
+# до кол-ва знаков (кол-во знаков передается вторым аргументом)
+# Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
+# Для решения задачи не используйте встроенные и функции и функции из модуля math
 
 
 def my_round(number, ndigits):
     number = list(str(number))
     x = ndigits+2
-    if int(number[x])>=5:
+    if int(number[x])>=5 and int(number[x-1])<9:
         del number[x:]
         c = int(number[x-1])+1
         number[x-1]= str(c)
         a = ''.join(number)
         a = float(a)
         return a
-    elif x>=9:
+    elif int(number[x-1])>= 9:
         del number[x-1:]
-        c = int(number[x])+1
-        number[x]= str(c)
+        c = int(number[x-2])+1
+        number[x-2]= str(c)
         a = ''.join(number)
         a = float(a)
-
+        if int(number[x-2])==10:
+            del number[x-1:]
+            c = 9
+            number[x-2]= str(c)
+            a = ''.join(number)
+            a = float(a)
+            return a
+        return a
     else:
         del number[x:]
         a = ''.join(number)
@@ -30,5 +42,5 @@ def my_round(number, ndigits):
 
 
 
-a = my_round(2.1234597, 6)
+a = my_round(2.1234997, 6)
 print(a)
